093c2a5683879c7631a182a812a1b2617fa07ef0
YANZOOPA <yahia.ni.19@gmail.com>
2024-01-30 14:35:47 +0100

    fx

--- javascript_objects_scopes_closures/0-rectangle.js
+++ javascript_objects_scopes_closures/0-rectangle.js
@@ -1,8 +1,5 @@
 class Rectangle {
-    // Empty class definition
-  }
-  
-  // Example of creating an instance of the Rectangle class
-  const myRectangle = new Rectangle();
-  console.log(myRectangle); // Output: Rectangle {}
+  // Empty class
+}
+
   
