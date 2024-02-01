df1f25f67c221d7e753d0d187a815479541707fc
YANZOOPA <yahia.ni.19@gmail.com>
2024-02-01 02:13:10 +0100

    js2fs2

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,14 +1,19 @@
 #!/usr/bin/node
 class Rectangle {
     constructor(w, h) {
+      // Convert negative values to positive
+      const positiveW = Math.abs(w);
+      const positiveH = Math.abs(h);
+  
       // Initialize the instance attributes width and height
-      if (w > 0 && Number.isInteger(w) && h > 0 && Number.isInteger(h)) {
-        this.width = w;
-        this.height = h;
+      if (positiveW > 0 && Number.isInteger(positiveW) && positiveH > 0 && Number.isInteger(positiveH)) {
+        this.width = positiveW;
+        this.height = positiveH;
       } else {
-        // If w or h is equal to 0 or not a positive integer, create an empty object
+        // If positiveW or positiveH is not a positive integer, create an empty object
         this.width = 0;
         this.height = 0;
       }
     }
   }
+  
