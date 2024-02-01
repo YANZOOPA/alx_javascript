9d32e133eb0126f064eab5f8e1221bcf43096156
YANZOOPA <yahia.ni.19@gmail.com>
2024-02-01 02:18:48 +0100

    ?

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,29 +1,19 @@
 #!/usr/bin/node
 class Rectangle {
-
     constructor(w, h) {
-  
       if (w > 0 && Number.isInteger(w)) {
-  
         this.width = w;
-  
       } else {
-  
         this.width = 0;
-  
       }
-  
       if (h > 0 && Number.isInteger(h)) {
-  
         this.height = h;
-  
       } else {
-  
         this.height = 0;
-  
       }
-  
     }
-  
   }
   
+  const r1 = new Rectangle(3, 3);
+  console.log(r1); // Output: Rectangle { width: 3, height: 3 }
+  console.log(r1.width, r1.height); // Output: 3 3  
