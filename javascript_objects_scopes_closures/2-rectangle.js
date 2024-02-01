7ea75a2ba81056c3782d0576c26c060a4b199f7e
YANZOOPA <yahia.ni.19@gmail.com>
2024-02-01 02:03:01 +0100

    js-2

---
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,13 @@
+class Rectangle {
+    constructor(w, h) {
+      // Initialize the instance attributes width and height
+      if (w > 0 && Number.isInteger(w) && h > 0 && Number.isInteger(h)) {
+        this.width = w;
+        this.height = h;
+      } else {
+        // If w or h is equal to 0 or not a positive integer, create an empty object
+        this.width = 0;
+        this.height = 0;
+      }
+    }
+  }
